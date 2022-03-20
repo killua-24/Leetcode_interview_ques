03df0c80d3e5676c04c7293c6942c9d4e99cc3e6
Nishchay Wankar <71606731+killua-24@users.noreply.github.com>
2022-03-20 09:58:59 +0530

    Time: 21 ms (40.00%), Space: 54.6 MB (20.00%) - LeetHub

---
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maximumSubsequenceCount(String s, String pattern) {
+       
+        long ans = 0, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < s.length(); ++i) {   
+            if (s.charAt(i) == pattern.charAt(1)) {   
+                ans += cnt1; 
+                cnt2++;
+            }
+            if (s.charAt(i) == pattern.charAt(0)) {   
+                cnt1++;
+            }
+        }
+        return ans + Math.max(cnt1, cnt2);
+    }
+    
+}
