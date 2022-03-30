242970cedae49e0199a059a7282d603e0d073e72
Nishchay Wankar <71606731+killua-24@users.noreply.github.com>
2022-03-30 10:38:23 +0530

    Added solution - LeetHub

---
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.java
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+        
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			long arr[] = new long[n];
+			
+			for(int i=0; i<n; i++)
+				arr[i] = sc.nextLong();
+		    out.println(new Solution().minCost(arr, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to return the minimum cost of connecting the ropes.
+    long minCost(long arr[], int n) 
+    {
+        if(n == 1) return 0;
+        PriorityQueue<Long> heap = new PriorityQueue<>();
+        
+        for(long it : arr){
+            heap.add(it);
+        }
+        
+        long ans = 0;
+        while(heap.size() >= 2){
+            long first = heap.poll();
+            long second = heap.poll();
+            long max = first + second;
+            
+            ans += max;
+            heap.add(max);
+        }
+        
+        return ans;
+    }
+}
